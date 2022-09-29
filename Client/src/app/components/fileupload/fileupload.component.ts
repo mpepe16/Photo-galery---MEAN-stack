784ef3be0e7ccdda9c02953167e9e5a332811453
mpepe16 <mpepe66@gmail.com>
2022-09-29 17:32:57 +0200

    Add: necessary properties to the FileuploadComponent including an IPictureModel service.

--- Client/src/app/components/fileupload/fileupload.component.ts
+++ Client/src/app/components/fileupload/fileupload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IPictureModel } from 'src/app/Services/file-preview-service.service';
 
 @Component({
   selector: 'atp-fileupload',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fileupload.component.scss']
 })
 export class FileuploadComponent implements OnInit {
+
+  protected imageSource!: IPictureModel | null;
+  protected message: any;
+  protected description!: string;
+  protected tags!: string;
+
 
   constructor() { }
 
