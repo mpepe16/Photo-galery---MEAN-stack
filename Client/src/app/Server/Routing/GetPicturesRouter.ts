8f62cce32062cc5df2201a768a30818261d5c21c
mpepe16 <mpepe66@gmail.com>
2022-10-06 11:12:18 +0200

    Modify: extend route get method with using the Picture distinct method to get all the picture with the given id.

--- Client/src/app/Server/Routing/GetPicturesRouter.ts
+++ Client/src/app/Server/Routing/GetPicturesRouter.ts
@@ -1,10 +1,17 @@
 import { IRouter } from "./IRouter";
+import { Request, Response } from "express";
+import { Picture, PictureSchema } from "../Database"
 
 export class GetPicturesRouter implements IRouter {
     public AddRoute(route: any): void {
         route.get('/get', 
         (request: Request, response: Response) => {
-
+            Picture.distinct("_id", (err: any, picture: any) => {
+                if(err) {
+                    response.send(err);
+                }
+                response.send(picture);
+            })
         })
     }
     
