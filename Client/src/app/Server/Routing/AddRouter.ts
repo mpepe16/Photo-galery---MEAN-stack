4c09ea45c71bda211c1df787988853fdff7a1da0
mpepe16 <mpepe66@gmail.com>
2022-10-06 09:55:17 +0200

    Add: comment to the AddRouter class.

--- Client/src/app/Server/Routing/AddRouter.ts
+++ Client/src/app/Server/Routing/AddRouter.ts
@@ -1,5 +1,7 @@
 import { IRouter } from "./IRouter";
 
+// This class will be responsible for handling the post request, ie adding the picture.
+
 export class AddPictureRouter implements IRouter {
     public AddRoute(route: any): void {
     route.post('/add/', (request: Request, response: Response) => {
