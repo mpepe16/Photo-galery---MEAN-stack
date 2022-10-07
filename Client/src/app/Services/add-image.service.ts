4e543017f5e526f50d19eac5f48e6a8e1a6fb438
mpepe16 <mpepe66@gmail.com>
2022-10-07 09:26:41 +0200

    Add: Extended add-image and load-image services.

---
+++ Client/src/app/Services/add-image.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { ContextServiceBase } from './ContextServiceBase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddImageService extends ContextServiceBase{
+
+  constructor() {
+    super();
+  }
+}
---
+++ Client/src/app/Services/load-image.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { ContextServiceBase } from './ContextServiceBase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadImageService extends ContextServiceBase{
+
+  constructor() {
+    super();
+  }
+}
